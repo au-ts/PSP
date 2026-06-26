b902d15e672fc30125bf6788f6418d486da10c0d
larrabyte <larrabyte@protonmail.ch>
2026-06-26 14:19:29 +1000

    Add .elf as the executable extension to align with LionsOS conventions

--- cmake/Modules/Platform/LionsOS.cmake
+++ cmake/Modules/Platform/LionsOS.cmake
@@ -1,3 +1,7 @@
 # The Microkit system description file expects self-contained
 # program images so we must compile everything statically
 set(CMAKE_EXE_EXPORTS_C_FLAG "-Wl,-static")
+
+set(CMAKE_EXECUTABLE_SUFFIX ".elf")
+set(CMAKE_SHARED_LIBRARY_SUFFIX ".so")
+set(CMAKE_SHARED_MODULE_SUFFIX ".elf")
